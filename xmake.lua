5e75208eeba7c7d1a478370e86e927a536fba571
ti-ji <1250079251@qq.com>
2021-11-13 18:02:39 +0800

    feat: 支持 HEAD~ .. HEAD~~ 的修改

--- xmake.lua
+++ xmake.lua
@@ -3,14 +3,9 @@ add_rules("mode.debug", "mode.release")
 target("gtk4_demo1")
     set_kind("binary")
     set_pcheader("src/stdafx.h")
-    set_rundir(".") -- 资源目录
     set_runenv("PATH","$(GTK4_BIN_PATH)", os.getenv("PATH"));
 
     add_files("src/**.c")
-    add_files("res/res.c")
-
-    add_includedirs("res") -- 资源文件
-
     add_defines("UNICODE", "_UNICODE")
 
     on_load(function(target)
@@ -28,7 +23,14 @@ target("gtk4_demo1")
 
         import("xmakefuns", {alias = "lx", rootdir= ".xmake"});
 
-        os.addenv("PKG_CONFIG_PATH", config.get("GTK4_DEBUG_PKG_CONFIG_PATH") or "")
+        if not os.isdir(path.join(target:targetdir(), "res")) then
+            local res_dir = path.join(path.relative(".", target:targetdir()), "res");
+            os.cd(target:targetdir());
+            os.ln(res_dir, "res");
+            os.cd(os.projectdir());
+        end
+
+        os.addenv("PKG_CONFIG_PATH", config.get("GTK4_DEBUG_PKG_CONFIG_PATH"))
 
         lx.need(target,
             {"pkgconfig::gtk4 >= 4.4.0"},
@@ -36,13 +38,17 @@ target("gtk4_demo1")
         );
 
         -- 无法判断文件是否被修改，如有必要，写成函数遍历资源文件和输出文件对比日期
-        os.run("glib-compile-resources --generate-header --sourcedir res res/res.xml")
-        os.run("glib-compile-resources --generate-source --sourcedir res res/res.xml")
+        os.run("glib-compile-resources --generate-header --sourcedir res res/res.xml --target src/res.h")
+        os.run("glib-compile-resources --generate-source --sourcedir res res/res.xml --target src/res.c")
 
         if is_host("windows") then
+            target:add("defines", "RESDIR=\"res/\"");
             target:add("links", "Gdi32") -- 加载字体函数
+        else
+            target:add("defines", "RESDIR=\"/usr/share/\"");
         end
 
+        target:add("cflags", "-g3", "-gdwarf-2");
     end)
 
     before_run(function()
